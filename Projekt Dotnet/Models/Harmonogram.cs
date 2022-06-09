41ee1ed287a816147610143fc6bdb3fa8f035765
Bartłomiej Chmielewski <gumif@wp.pl>
2022-06-09 11:41:15 +0200

    trolo

---
+++ Projekt Dotnet/Models/Harmonogram.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projekt_Dotnet.Models
+{
+    public class Harmonogram
+    {
+        [Required]
+        private int Id;
+        [Required]
+        public string HowManyDays;
+        [Required]
+        public int HowManyHours;
+        [Required]
+        public string Duties;
+        public void AddDuty(string newDuty)
+        {
+            Duties = Duties + ", " + newDuty;
+        }
+    }
+}
